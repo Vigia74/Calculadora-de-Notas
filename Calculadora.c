2cef6e9acfb3ca7fd6c296cb8569c45a24b6be44
unknown <Rafael Lemos>
2025-10-23 10:56:32 -0300

    Calculadora De Notas

---
+++ Calculadora.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, n3, n4;
+    int divi = 4;
+    int calc = 0;
+
+
+    printf("Adicione as Notas ao sistema.\n");
+    printf("Nota 1\n");
+    scanf("%d", &n1);
+    printf("Nota 2\n");
+    scanf("%d", &n2);
+    printf("Nota 3\n");
+    scanf("%d", &n3);
+    printf("Nota 4\n");
+    scanf("%d", &n4);
+
+    calc = (n1 + n2 + n3 + n4) / divi;
+
+    if (calc <= 6){
+        printf("Voce foi reprovado com a media de %d.", calc);
+    }
+
+    else if (calc >= 7){
+        printf("Voce foi aprovado com a media de %d.", calc);
+    }
+
+}
